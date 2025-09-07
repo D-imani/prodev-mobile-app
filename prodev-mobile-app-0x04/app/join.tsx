da9eb891183f2b5e4b23e9f63e44f89c6f69c4b9
Dorna <dorna.imani943@gmail.com>
2025-09-07 20:37:44 +0300

    upadted details

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import { styles } from "@/styles/_joinstyle";
+import { styles } from "@/styles/_join";
 import { HEROLOGO } from "@/constants";
 
 export default function Index() {
